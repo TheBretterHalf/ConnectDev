b1964dda078edf291db7afc2c58936dfe6dbf432
unknown <brettstainken.student@careerdevs.com>
2019-09-07 19:56:17 -0400

    adding password validation

---
+++ routes/api/users.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const router = express.Router();
+const {
+    check,
+    validationResult
+} = require('express-validator');
+
+//@route    POST api/users
+//@desc     Register User
+//@access   Public
+router.post(
+    '/',
+    [
+        check('name', 'name is required')
+        .not()
+        .isEmpty(),
+        check('email', 'Please include a valid email').isEmail(),
+        check('password', 'Please enter a password with 6 or more characters').isLength({
+            min: 6
+        })
+    ],
+    (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({
+                errors: errors.array()
+            });
+        }
+
+        //see if user exists
+
+        //get users gravatar
+
+        //encrypt password 
+
+        //return json webtoken
+        res.send('User route')
+    });
+
+module.exports = router;
